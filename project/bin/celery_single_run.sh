d21a5531e8ad503c73b647b6b84f1c121d34c71b
Steven Skoczen <steven@quantumimagery.com>
2012-11-24 14:04:12 -0800

    live settings

--- project/bin/celery_single_run.sh
+++ project/bin/celery_single_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cmd="python project/manage.py celeryd -B "
+cmd="python project/manage.py celeryd -B --settings=envs.live"
 $cmd &
 sleep 10
 ps aux | grep 'manage.py celeryd' | awk '{print $2}' | xargs kill -TERM
