2314a9ed35827aac13532041c104a1f39bbfdaf2
Steven Skoczen <steven@quantumimagery.com>
2013-04-03 16:30:25 -0700

    resume largely good, missing font awesome.

--- project/apps/resume/views.py
+++ project/apps/resume/views.py
@@ -7,11 +7,14 @@ def home(request):
     skill_values = OrderedDict([
         ("UX", 7),
         ("UI", 6),
-        ("Design", 5),
+        ("Design", 4),
         ("Front-end", 9),
         ("Back-end", 8),
         ("Devops", 6),
-        ("DBA", 4),
-        ("Server-admin", 4),
+        ("DBA", 3),
+        ("Server admin", 3),
+        ("Project management", 5),
     ])
+    num_skills = len(skill_values.keys())
+
     return locals()
