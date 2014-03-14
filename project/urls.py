9fb72a4f5c67598908726a26eb05e968707dad06
Steven Skoczen <steven@quantumimagery.com>
2014-03-13 20:10:18 -0700

    Adds MEDIA_URL, more fluff.

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
@@ -17,4 +18,4 @@ urlpatterns = patterns('',
     }),
     url(r'^admin/', include(admin.site.urls)),
 )
-    
+urlpatterns += staticfiles_urlpatterns()
