ecd1237369b8f62de0ba79351e2c2c02859035b1
Steven Skoczen <steven@quantumimagery.com>
2014-03-13 20:01:38 -0700

    Pulls ADMIN_MEDIA_PREFIX, adds runtime.txt

--- project/envs/common.py
+++ project/envs/common.py
@@ -55,7 +55,7 @@ STATICFILES_FINDERS = (
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+# ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 
 # Make this unique, and don't share it with anybody.
