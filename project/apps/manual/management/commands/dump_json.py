26948b6409ee10c86a53f1f1091f5697d45a2bc9
Steven Skoczen <steven@quantumimagery.com>
2014-05-07 19:09:10 -0700

    Adds correlator and dump functions

---
+++ project/apps/manual/management/commands/dump_json.py
@@ -0,0 +1,96 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+import pickle
+import logging
+logger = logging.getLogger("foo")
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+
+from manual.models import GutterBumper
+
+class Command(BaseCommand):
+
+    def append_to_data(self, index, value):
+        if self.first_run:
+            try:
+                float(value)
+                return True
+            except:
+                self.valid_data = False
+                logger.error("Error converting %s to float, skipping this row. (Index %s, %s)" % (value, index, self.b))
+                return False
+        try:
+            self.data[index].append(float(value))
+        except:
+            raise Exception("Error converting %s to float. (Index %s)" % (value, index))
+
+    def handle(self, *args, **options):
+        self.data = []
+        for d in range(0, 26):
+            self.data.append([])
+        for b in GutterBumper.objects.all().order_by("date"):
+            self.first_run = True
+            self.valid_data = True
+            self.add_valid_data = False
+            self.b = b
+            while self.first_run or self.add_valid_data:
+                self.append_to_data(0, b.date.month) # month
+                self.append_to_data(1, b.woke_up_at.hour)  # woke up hour
+                self.append_to_data(2, b.fell_asleep_at.hour)  #  = models.TimeField(default=datetime.time(0, 00))
+                self.append_to_data(3, b.sleep_hrs)  #  = models.FloatField(default=0, blank=True, null=True, verbose_name="Sleep", help_text="Sleep this morning")
+                self.append_to_data(4, b.work_hrs)  #  = models.FloatField(default=0, blank=True, null=True, verbose_name="Work")
+                self.append_to_data(5, b.alone_hrs)  #  = models.FloatField(default=0, blank=True, null=True, verbose_name="Alone")
+                self.append_to_data(6, b.friend_hrs)  #  = models.FloatField(default=0, blank=True, null=True, verbose_name="Friend")
+                self.append_to_data(7, b.public_hrs)  #  = models.FloatField(default=0, blank=True, null=True, help_text="Not specifically hanging with people, but in a larger crowd", verbose_name="Public")
+                self.append_to_data(8, b.relationship_hrs)  #  = models.FloatField(default=0, blank=True, null=True, verbose_name="Relationship")
+                self.append_to_data(9, 10 if b.off else 0)  #  = models.BooleanField(default=False)
+                self.append_to_data(10, b.sex or 0)  #  = models.IntegerField(default=0)
+                self.append_to_data(11, 10 if b.interacted_with_art else 0)  #  = models.BooleanField(default=False)
+                self.append_to_data(12, 10 if b.worked_out else 0)  #  = models.BooleanField(default=False)
+                self.append_to_data(13, 10 if b.meditated else 0)  #  = models.BooleanField(default=False, verbose_name="meditated")
+                self.append_to_data(14, 10 if b.left_the_house else 0)  #  = models.BooleanField(default=False)
+                self.append_to_data(15, 10 if b.nature_time else 0)  #  = models.BooleanField(default=False)
+                self.append_to_data(16, 10 if b.inbox_zero else 0)  #  = models.BooleanField(default=False)
+                self.append_to_data(17, 10 if b.travelling_or_out_of_routine else 0)  #  = models.BooleanField(default=False, verbose_name="Travelling/Nonroutine")
+                self.append_to_data(18, b.number_of_sleep_beers or 0)  #  = models.IntegerField(blank=True, null=True, verbose_name="# of sleep beers")
+                self.append_to_data(19, b.number_of_fun_beers or 0)  #  = models.IntegerField(blank=True, null=True, verbose_name="# of fun beers")
+                self.append_to_data(20, b.presence)  #  = models.IntegerField(blank=True, null=True, help_text="1-10")
+                self.append_to_data(21, b.happiness)  #  = models.IntegerField(blank=True, null=True, help_text="1-10")
+                self.append_to_data(22, b.creativity)  #  = models.IntegerField(blank=True, null=True, help_text="1-10")
+                self.append_to_data(23, b.morning_mood)  #  = models.IntegerField(blank=True, null=True, help_text="1-10")
+                self.append_to_data(24, b.unbusy or 9)  #  = models.IntegerField(blank=True, null=True, help_text="1-10")
+                notes_len = 0
+                try:
+                    notes_len = len(b.notes)
+                except:
+                    pass
+                self.append_to_data(25, notes_len)  #  = models.TextField(blank=True, null=True, default="86400")
+                if self.first_run:
+                    self.first_run = False
+
+                    if self.valid_data:
+                        self.add_valid_data = True
+                else:
+                    self.add_valid_data = False
+
+        # Sanity check.
+        total_len = None
+        for index in range(0, len(self.data)):
+            d = self.data[index]
+            if not total_len:
+                total_len = len(d)
+
+            if len(d) != total_len:
+                logger.critical("Unequal datasets %s!" % index)
+
+        print pickle.dumps(self.data)
---
+++ source/correlate/correlate.py
@@ -0,0 +1,55 @@
+import json
+import requests
+import pickle
+
+data = pickle.load(open("data_sample.json"))
+
+cols = [
+    "date",
+    "woke_up_at",
+    "fell_asleep_at",
+    "sleep_hrs",
+    "work_hrs",
+    "alone_hrs",
+    "friend_hrs",
+    "public_hrs",
+    "relationship_hrs",
+    "off",
+    "sex",
+    "interacted_with_art",
+    "worked_out",
+    "meditated",
+    "left_the_house",
+    "nature_time",
+    "inbox_zero",
+    "travelling_or_out_of_routine",
+    "number_of_sleep_beers",
+    "number_of_fun_beers",
+    "presence",
+    "happiness",
+    "creativity",
+    "morning_mood",
+    "unbusy",
+    "notes length",
+]
+
+headers = {'Content-type': 'application/json', }
+resp = requests.post("http://correlationbot.com", headers=headers, data=json.dumps({
+    "data": data
+}))
+print resp
+print resp.content
+correlations = resp.json()["correlations"]
+for c in correlations:
+    print c["column_1"]
+    print c["column_2"]
+    c["col1"] = cols[c["column_1"]-1]
+    c["col2"] = cols[c["column_2"]-1]
+correlations.sort(key=lambda x: x["pearson"], reverse=True)
+print correlations
+for c in correlations:
+    print "%s<->%s   %s" % (
+        c["col1"],
+        c["col2"],
+        c["pearson"],
+    )
