64716c4b04d83bd5611343ab1667a03baaec39d0
Steven Skoczen <steven@quantumimagery.com>
2014-05-19 23:12:10 -0700

    Passing tests.

--- project/envs/common.py
+++ project/envs/common.py
@@ -105,6 +105,7 @@ INSTALLED_APPS = (
     "compressor",
     'djcelery',
     "django_extensions",
+    "django_nose",
     "import_export",
     # "lettuce.django",
     "gunicorn",
@@ -125,6 +126,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.request",
 )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 import djcelery
 djcelery.setup_loader()
